d3069e9e446aae898fbeb71cccbcc30a1e640ddc
mcdonaldduncan <380939@colum.edu>
2022-01-23 12:05:04 -0600

    project one

---
+++ WorkshopOne/WorkshopOne/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopOne
+{
+    internal class Game
+    {
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        public void Play()
+        {
+            player1.name = "Connor";
+            player2.name = "Lindsey";
+            player1.score = 5;
+            player2.score = 7;
+
+            Console.WriteLine(player1.name);
+            Console.WriteLine(player2.name);
+            Console.WriteLine(player1.score);
+            Console.WriteLine(player2.score);
+        }
+    }
+}
---
+++ WorkshopOne/WorkshopOne/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopOne
+{
+    internal class Player
+    {
+        public string name;
+        public int score;
+    }
+}
