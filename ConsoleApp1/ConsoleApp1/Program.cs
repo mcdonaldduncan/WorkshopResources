1e231b10e4539a9d48784d967136c95e8a0fea80
Duncan McDonald <72177724+mcdonaldduncan@users.noreply.github.com>
2022-01-25 14:55:44 -0600

    add beginner proj

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student duncan = new Student("Duncan", "He/Him/His", "Programming, Reading, Swimming and Video Games");
+            duncan.Description();
+            Console.ReadKey();
+        }
+
+        private class Student
+        {
+            string name;
+            string pronouns;
+            string hobbies;
+
+
+            public Student(string n, string p, string h)
+            {
+                name = n;
+                pronouns = p;
+                hobbies = h;
+            }
+
+            public void Description()
+            {
+                Console.WriteLine($"Hi, my name is {name}, my pronouns are {pronouns} and I enjoy {hobbies}.");
+            }
+        }
+    }
+}
