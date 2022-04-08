77f6074117d63716453cc88d100240ebd1c2c421
mcdonaldduncan <380939@colum.edu>
2022-04-08 08:18:55 -0500

    update

--- Node Pathfinder/Assets/Scripts/NodePathfinder.cs
+++ Node Pathfinder/Assets/Scripts/NodePathfinder.cs
@@ -7,14 +7,21 @@ public class NodePathfinder : MonoBehaviour
     [SerializeField] Transform[] nodes;
     [SerializeField] float speed;
     [SerializeField] int index;
+
+    [System.NonSerialized] public bool shouldDestroy;
+    [System.NonSerialized] public bool shouldMove;
+
     void Start()
     {
         index = 0;
+        shouldDestroy = false;
+        shouldMove = false;
     }
 
     void Update()
     {
-        FollowNodes();
+        if (shouldMove)
+            FollowNodes();
     }
 
     void FollowNodes()
@@ -22,7 +29,7 @@ public class NodePathfinder : MonoBehaviour
         if (Vector3.Distance(transform.position, nodes[index].position) < 1f)
             index++;
         if (index == nodes.Length)
-            index = 0;
+            shouldDestroy = true;
 
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, nodes[index].position, step);
