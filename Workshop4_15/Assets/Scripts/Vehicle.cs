bebdef5c40af8d9a6f794fce29ee99e2b52d51bb
mcdonaldduncan <380939@colum.edu>
2022-04-30 19:17:27 -0500

    update

--- Workshop4_15/Assets/Scripts/Vehicle.cs
+++ Workshop4_15/Assets/Scripts/Vehicle.cs
@@ -25,10 +25,11 @@ public class Vehicle : MonoBehaviour
         Vector2 desired = target.position - transform.position;
         //desired /= desired.magnitude;
         //desired *= maxSpeed;
-        float x = Mathf.Clamp(desired.x, -maxSpeed, maxSpeed);
-        float y = Mathf.Clamp(desired.y, -maxSpeed, maxSpeed);
-        Vector2 clampedDesired = new Vector2(x, y);
-        Vector2 steer = clampedDesired - (Vector2)rb.velocity;
+        desired = Vector2.ClampMagnitude(desired, maxSpeed);
+        //float x = Mathf.Clamp(desired.x, -maxSpeed, maxSpeed);
+        //float y = Mathf.Clamp(desired.y, -maxSpeed, maxSpeed);
+        //Vector2 clampedDesired = new Vector2(x, y);
+        Vector2 steer = desired - (Vector2)rb.velocity;
         rb.AddForce(steer, ForceMode.Acceleration);
        
     }
