cf626d5af94537566164e43326df13790f184310
Duncan McDonald <72177724+mcdonaldduncan@users.noreply.github.com>
2022-09-09 12:19:20 -0500

    update

--- Workshop4_15/Assets/Scripts/Vehicle.cs
+++ Workshop4_15/Assets/Scripts/Vehicle.cs
@@ -23,14 +23,14 @@ public class Vehicle : MonoBehaviour
     void Follow()
     {
         Vector2 desired = target.position - transform.position;
-        //desired /= desired.magnitude;
-        //desired *= maxSpeed;
+        desired /= desired.magnitude;
+        desired *= maxSpeed;
         desired = Vector2.ClampMagnitude(desired, maxSpeed);
-        //float x = Mathf.Clamp(desired.x, -maxSpeed, maxSpeed);
-        //float y = Mathf.Clamp(desired.y, -maxSpeed, maxSpeed);
-        //Vector2 clampedDesired = new Vector2(x, y);
+        float x = Mathf.Clamp(desired.x, -maxSpeed, maxSpeed);
+        float y = Mathf.Clamp(desired.y, -maxSpeed, maxSpeed);
+        Vector2 clampedDesired = new Vector2(x, y);
         Vector2 steer = desired - (Vector2)rb.velocity;
         rb.AddForce(steer, ForceMode.Acceleration);
-       
+
     }
 }
