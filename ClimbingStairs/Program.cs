715da229f4e309216d72648537febdb9e3e47840
mcdonaldduncan <380939@colum.edu>
2022-09-09 21:35:00 -0500

    update

--- ClimbingStairs/Program.cs
+++ ClimbingStairs/Program.cs
@@ -12,16 +12,37 @@ namespace ClimbingStairs
         {
             int permutations = 0;
 
+            Dictionary<int, int> cache = new Dictionary<int, int>();
+
+
+            int ClimbStairs(int n)
+            {
+                int p1 = 1; 
+                int p2 = 1;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int temp = p1;
+                    p1 += p2;
+                    p2 = temp;
+                }
+
+                return p1;
+            }
+
+
+
+            // Brute Force
             Console.WriteLine("Enter the number of stairs: ");
             int stairs = Convert.ToInt32(Console.ReadLine());
-            FindPermutations(stairs);
-            Console.WriteLine("The number of permutations is: " + permutations);
+            //FindPermutations(stairs);
+            Console.WriteLine("The number of permutations is: " + ClimbStairs(stairs));
+            Console.ReadLine();
 
             void FindPermutations(int n)
             {
                 IterateTree(0, n);
             }
-            
+
             void IterateTree(int current, int target)
             {
                 if (current > target)
@@ -38,7 +59,7 @@ namespace ClimbingStairs
                 IterateTree(i, target);
                 IterateTree(j, target);
             }
-            Console.ReadLine();
+
         }
     }
 }
