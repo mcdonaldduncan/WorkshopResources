fedd85ca56624b0b59cee03ed19dc10194d4e26b
mcdonaldduncan <380939@colum.edu>
2022-09-16 17:03:49 -0500

    update

--- FundamentalsWeek2/DataSorting.cs
+++ FundamentalsWeek2/DataSorting.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using static FundamentalsWeek2.Printing;
 
 namespace FundamentalsWeek2
@@ -10,7 +11,6 @@ namespace FundamentalsWeek2
         internal static void SortData()
         {
             GameInfo info = new GameInfo();
-            List<Info> metaData = info.MetaData;
             List<string> mapNameBank = new List<string>();
             string mostCommonGenre;
             int mostCommonGenreCount;
@@ -18,33 +18,39 @@ namespace FundamentalsWeek2
 
 
             // Number of games
-            Print($"Total Entries: {metaData.Count}");
+            Print($"Total Entries: {info.MetaData.Count}");
             Print();
 
 
 
             // Most frequent genre
-            mostCommonGenre = metaData.Select(x => x.Genre).OrderBy(y => y.Count()).First();
-            mostCommonGenreCount = metaData.Count(x => x.Genre == mostCommonGenre);
+            mostCommonGenre = info.MetaData.Select(x => x.Genre).OrderBy(y => y.Count()).First();
+            mostCommonGenreCount = info.MetaData.Count(x => x.Genre == mostCommonGenre);
             Print($"The most frequent genre is {mostCommonGenre} with a total of {mostCommonGenreCount} entries!");
             Print();
 
 
 
             // Map names with most characters excluding spaces with which game they belong to
-            metaData.Select(x => x.MapNames).ToList().ForEach(y => mapNameBank.AddRange(y));
-            mapNameBank.OrderByDescending(x => x.Trim().Length).ToList().ForEach(y => Print($"Map name: {y}, length without spaces: {y.Trim().Length}, Game: {FindGameName(y)}"));
+            // Regular Expression example from https://stackoverflow.com/questions/6219454/efficient-way-to-remove-all-whitespace-from-string
+            info.MetaData.Select(x => x.MapNames).ToList().ForEach(y => mapNameBank.AddRange(y));
+            mapNameBank.OrderByDescending(x => RemoveWhiteSpace(x).Length).ToList().ForEach(y => Print($"Map name: {y}, length without spaces: {RemoveWhiteSpace(y).Length}, Game: {FindGameName(y)}"));
             Print();
+            
+            string RemoveWhiteSpace(string x)
+            {
+                return Regex.Replace(x, @"\s+", "");
+            }
 
             string FindGameName(string name)
             {
-                return metaData.SelectMany(x => x.MapNames.Where(y => name == y).Select(z => x)).First().Name;
+                return info.MetaData.SelectMany(x => x.MapNames.Where(y => name == y).Select(z => x)).First().Name;
             }
             
 
 
             // Dictionary with KVP ID : INFO, display via loop
-            var newDict = metaData.ToDictionary(x => x.Id);
+            var newDict = info.MetaData.ToDictionary(x => x.Id);
             Print("Dictionary with kvp of id : info, displayed with loop:");
             foreach (var kvp in newDict)
             {
@@ -64,7 +70,7 @@ namespace FundamentalsWeek2
 
             // Map names that have the letter z in them
             Print("Map names that contain z: ");
-            metaData.ForEach(x => x.MapNames.Where(y => y.Contains("z")).ToList().ForEach(z => Print(z)));
+            info.MetaData.ForEach(x => x.MapNames.Where(y => y.Contains("z")).ToList().ForEach(z => Print(z)));
 
 
             Console.ReadLine();
--- FundamentalsWeek2/Program.cs
+++ FundamentalsWeek2/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using static FundamentalsWeek2.DataSorting;
 
 namespace FundamentalsWeek2
